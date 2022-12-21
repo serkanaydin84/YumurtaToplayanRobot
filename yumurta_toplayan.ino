85132597f91b9a075f2162942ba97582d9ae0d2f
DESKTOP-QP70SMH\Robotik <123@123.com>
2022-12-21 19:28:16 +0300

    no message

--- yumurta_toplayan.ino
+++ yumurta_toplayan.ino
@@ -1,21 +1,28 @@
 #include <Servo.h>
 
+// renk sensörü tanımlamaları
 #define csOrta_s0 13
 #define csOrta_s1 12
-#define csOrta_s2 11
-#define csOrta_s3 10
-#define csOrta_out 9
+#define csOrta_s2 8
+#define csOrta_s3 7
+#define csOrta_out 4
+#define csArka_out 2
 
+//  RGB LED için kullanıyor
 #define kled 7
 #define yled 6
 #define mled 5
 
 Servo servoOrta;
-int degree;
-int kirmizi, yesil, mavi = 0;
+Servo servoArka;
+
+int degreeOrta;
+int yumurtaKirmizi, yumurtaYesil, yumurtaMavi = 0;
+int zeminKirmizi, zeminYesil, zeminMavi = 0;
 
 String bizimRengimiz = "kirmizi";  // arkadaki renk sensörünün okuduğu değer atanacak
 String yumurtaRengi;    // ortadaki renk sensörünün okuduğu değer atanacak
+String zeminRengi;    // arkadaki renk sensörünün okuduğu değer atanacak
 
 void setup() {
   pinMode(csOrta_s0, OUTPUT);
@@ -23,6 +30,7 @@ void setup() {
   pinMode(csOrta_s2, OUTPUT);
   pinMode(csOrta_s3, OUTPUT);
   pinMode(csOrta_out, INPUT);
+  pinMode(csArka_out, INPUT);
   pinMode(kled, OUTPUT);
   pinMode(yled, OUTPUT);
   pinMode(mled, OUTPUT);
@@ -32,22 +40,23 @@ void setup() {
   digitalWrite(csOrta_s1, LOW);
   Serial.begin(9600);
 
-  // Orta Servo tanımlanıyor
-  servoOrta.attach(3);
+  
+  servoOrta.attach(3);    // Orta Servo tanımlanıyor
+  servoArka.attach(9);    // Arka Servo tanımlanıyor
 }
 
 void loop() {
   //kalibreEt();
   kalibreEdilmis();
-  yumurtaRengiOKu();
+  yumurtaRengiOku();
 
   //servoOrtaKalibreEt();
   servoOrtaDon(yumurtaRengi);
 }
 
 void servoOrtaKalibreEt() {
-  degree = 90;
-  servoOrta.write(degree);
+  degreeOrta = 90;
+  servoOrta.write(degreeOrta);
 }
 
 // gelen renk değerlerine göre orta servo ayarlanıyor
@@ -56,14 +65,14 @@ void servoOrtaKalibreEt() {
 // mavi = sağ dönme işlemleri yapılıyor
 void servoOrtaDon(String renk) {
   if (renk == "beyaz") {  // düz ol
-    degree = 90;
-    servoOrta.write(degree);
+    degreeOrta = 90;
+    servoOrta.write(degreeOrta);
   } else if (bizimRengimiz == renk) {  // sola dön
-    degree = 180;
-    servoOrta.write(degree);
+    degreeOrta = 180;
+    servoOrta.write(degreeOrta);
   } else {  // sağa dön
-    degree = 0;
-    servoOrta.write(degree);
+    degreeOrta = 0;
+    servoOrta.write(degreeOrta);
   }
 }
 
@@ -71,9 +80,9 @@ void kalibreEt() {
   // Kırmızı rengi belirleme
   digitalWrite(csOrta_s2, LOW);
   digitalWrite(csOrta_s3, LOW);
-  kirmizi = pulseIn(csOrta_out, LOW);
+  yumurtaKirmizi = pulseIn(csOrta_out, LOW);
   Serial.print("Kırmızı renk: ");
-  Serial.print(kirmizi);
+  Serial.print(yumurtaKirmizi);
   Serial.print("\t");
   delay(1000);
 
@@ -81,9 +90,9 @@ void kalibreEt() {
   // Yesil rengi belirleme
   digitalWrite(csOrta_s2, HIGH);
   digitalWrite(csOrta_s3, HIGH);
-  yesil = pulseIn(csOrta_out, LOW);
+  yumurtaYesil = pulseIn(csOrta_out, LOW);
   Serial.print("Yeşil renk: ");
-  Serial.print(yesil);
+  Serial.print(yumurtaYesil);
   Serial.print("\t");
   delay(1000);
 
@@ -91,9 +100,9 @@ void kalibreEt() {
   // Mavi rengi belirleme
   digitalWrite(csOrta_s2, LOW);
   digitalWrite(csOrta_s3, HIGH);
-  mavi = pulseIn(csOrta_out, LOW);
+  yumurtaMavi = pulseIn(csOrta_out, LOW);
   Serial.print("Mavi renk: ");
-  Serial.print(mavi);
+  Serial.print(yumurtaMavi);
   Serial.println("\t");
   delay(1000);
 }
@@ -103,39 +112,39 @@ void kalibreEdilmis() {
   // Kırmızı rengi belirleme
   digitalWrite(csOrta_s2, LOW);
   digitalWrite(csOrta_s3, LOW);
-  kirmizi = pulseIn(csOrta_out, LOW);
-  kirmizi = map(kirmizi, 48, 180, 0, 100);
+  yumurtaKirmizi = pulseIn(csOrta_out, LOW);
+  yumurtaKirmizi = map(yumurtaKirmizi, 48, 180, 0, 100);
   Serial.print("Kırmızı renk: ");
-  Serial.print(kirmizi);
+  Serial.print(yumurtaKirmizi);
   Serial.print("\t");
-  delay(1000);
+  delay(50);
 
   
   // Yesil rengi belirleme
   digitalWrite(csOrta_s2, HIGH);
   digitalWrite(csOrta_s3, HIGH);
-  yesil = pulseIn(csOrta_out, LOW);
-  yesil = map(yesil, 43, 210, 0, 100);
+  yumurtaYesil = pulseIn(csOrta_out, LOW);
+  yumurtaYesil = map(yumurtaYesil, 43, 210, 0, 100);
   Serial.print("Yeşil renk: ");
-  Serial.print(yesil);
+  Serial.print(yumurtaYesil);
   Serial.print("\t");
-  delay(1000);
+  delay(50);
 
   
   // Mavi rengi belirleme
   digitalWrite(csOrta_s2, LOW);
   digitalWrite(csOrta_s3, HIGH);
-  mavi = pulseIn(csOrta_out, LOW);
-  mavi = map(mavi, 40, 190, 0, 100);
+  yumurtaMavi = pulseIn(csOrta_out, LOW);
+  yumurtaMavi = map(yumurtaMavi, 40, 190, 0, 100);
   Serial.print("Mavi renk: ");
-  Serial.print(mavi);
+  Serial.print(yumurtaMavi);
   Serial.println("\t");
-  delay(1000);
+  delay(50);
 }
 
-void yumurtaRengiOKu() {
+void yumurtaRengiOku() {
   // beyaz algılanıyor
-  if (kirmizi < 0 and mavi < 0 and yesil < 0) {
+  if (yumurtaKirmizi < 0 and yumurtaMavi < 0 and yumurtaYesil < 0) {
     digitalWrite(kled, HIGH);
     digitalWrite(yled, HIGH);
     digitalWrite(mled, HIGH);
@@ -143,7 +152,7 @@ void yumurtaRengiOKu() {
     Serial.println("Beyaz zemin algılandı");
   }
   // kırmızı algılanıyor
-  else if (kirmizi<90 and kirmizi < mavi and kirmizi < yesil) {
+  else if (yumurtaKirmizi<90 and yumurtaKirmizi < yumurtaMavi and yumurtaKirmizi < yumurtaYesil) {
     digitalWrite(kled, HIGH);
     digitalWrite(yled, LOW);
     digitalWrite(mled, LOW);
@@ -151,7 +160,7 @@ void yumurtaRengiOKu() {
     Serial.println("Kırmızı yumurta algılandı");
   }
   // mavi algılanıyor
-  else if (mavi < kirmizi and mavi < yesil ) {
+  else if (yumurtaMavi < yumurtaKirmizi and yumurtaMavi < yumurtaYesil ) {
     digitalWrite(mled, HIGH);
     digitalWrite(kled, LOW);
     digitalWrite(yled, LOW);
